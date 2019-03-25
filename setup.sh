fca2ef255c2203450dd20f13595743162ed87247
johannessen <git@arne.johannessen.de>
2019-03-25 20:45:41 +0100

    deploy intern2 Regeln-XML src through github clone

--- setup.sh
+++ setup.sh
@@ -578,11 +578,13 @@ setup_copy /etc/init.d/skgb-intern.sh X
 
 echo "Installing SKGB-intern into /srv/intern and /srv/legacy ..."
 sudo -u aj -- git clone https://github.com/skgb/intern.git
+sudo -u aj -- git clone https://github.com/skgb/regeln.git intern/public/regeln
+sudo -u aj -- ln -s src/Stander.svg intern/public/regeln/Stander.svg
+sudo -u aj -- ln -s src/regeln2html.xsl intern/public/regeln/regeln2html.xsl
 tar -xf "$BACKUPSRVPATH" intern/skgb-intern.production.conf
 mv intern/templates/content/stegdienstliste.html.ep intern/templates/content/stegdienstliste.html.development.ep
 tar -xf "$BACKUPSRVPATH" intern/templates/content/stegdienstliste.html.ep
 tar -xf "$BACKUPSRVPATH" intern/public/Merkblatt\ Datenschutz.pdf
-tar -xf "$BACKUPSRVPATH" intern/public/regeln/src-copy
 tar -xf "$BACKUPSRVPATH" intern/public/lib
 # see https://github.com/oetiker/mojolicious-plugin-reverseproxy/issues/5
 tar -xvf "$BACKUPSRVPATH" intern/lib/Mojolicious/Plugin/ReverseProxy.pm
